e0d98be48a419a16b1e650e3da1fd446286f0474
Zenon Trujillo <zenon.t@outlook.com>
2023-08-28 21:27:26 -0700

    chore: standing up host

---
+++ web/host/module-federation.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'web-host',
+  remotes: [],
+};
---
+++ web/host/module-federation.server.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('@nx/devkit').ModuleFederationConfig}
+ **/
+const moduleFederationConfig = {
+  name: 'web-host',
+  remotes: [],
+};
+
+module.exports = moduleFederationConfig;
